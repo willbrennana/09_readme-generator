89ac182339bc8f35c02efaa6c093f4c5eaa1dda0
willbrennana <will.brennana@gmail.com>
2023-01-02 00:51:34 -0600

    1.1.2 Updates: Installed npm Added inquirer const as global variable Console logged init function using one sample question/object to test functionality Added sample question/object to questions const Updated init function to call response data for README.md file Added fs const as global variable Added return method to writeToFile function Added generateMarkdown const as global variable Added exportData const to writeToFile and added "This is the title..." to generateMarkdown.js file Lowercased "t" in "title" within test object Added return method to renderLicenseBadge in gernerateMarkdown.js file

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "title",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const exportData = generateMarkdown(data);
+  return fs.writeFileSync(fileName, exportData);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    writeToFile("README.md", response);
+  });
+}
 
 // Function call to initialize app
 init();
